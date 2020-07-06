47853e164c12f26407fb9584571ee146e5571fc9
chencailong <chencailong@chargefrog.com>
2020-07-06 14:24:39 +0800

    尝试新增添加页

--- pages/we_service/trivia_books/add.vue
+++ pages/we_service/trivia_books/add.vue
@@ -1,7 +1,33 @@
 <template>
 	<view class="chenyu_page">
 		<view class="category">
-			
+			<u-form>
+				<!-- 类别选择 -->
+				<u-form-item label-position="top" label="选择类型" label-width="150">
+					<u-radio-group v-model="select_category" @change="click_category" :wrap="false">
+						<u-radio 
+							shape="circle" 
+							v-model="item.id" 
+							v-for="(item, index) in category_list" 
+							:key="index" 
+							:name="item.id">{{ item.name }}</u-radio>
+					</u-radio-group>
+				</u-form-item>
+				<!-- 平分式 -->
+				<block v-if="select_category==1">
+					<u-form-item label-width="150" label-position="left" label="便签名称">
+						<u-input placeholder="请输入名称" v-model="info.name" type="text" />
+					</u-form-item>
+				</block>
+				<!-- 随机式 -->
+				<block v-if="select_category==2">
+					
+				</block>
+				<!-- 轮流式 -->
+				<block v-if="select_category==3">
+					
+				</block>
+			</u-form>
 		</view>
 	</view>
 </template>
@@ -11,21 +37,35 @@
 	const instance = new request()
 	const app = getApp()
 	export default {
-		//声明引用组件
-		components:{},
+		
+		components:{//声明引用组件
+			
+		},
 		data() {
 			return {
-				category_list:[
-					{	name:"随机式"	},
-					{	name:"平分式"	},
-					{	name:"轮流式"	}
-				]
+				category_list:[//当前可选择类型
+					{	name:"平分式",
+						id:1
+					},
+					{	name:"随机式",
+						id:2
+					},
+					{	name:"轮流式",
+						id:3
+					},
+				],
+				select_category:1,
+				info:{
+					name:""
+				},
 			}
 		},
-		//计算属性
-		computed:{},
-		//检测属性
-		watch:{},
+		computed:{//计算属性
+			
+		},	
+		watch:{//检测属性
+			
+		},
 		onReady() {},
 		onShow() {},
 		onLoad(options) {
@@ -33,7 +73,9 @@
 		},
 		onHide() {},
 		methods:{
-			
+			click_category(e){
+				this.select_category = e
+			}
 		}
 	}
 </script>
