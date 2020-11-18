f6fc397448e2b33ba4b9eb5879c82bfbe4f59e27
chencailong <chencailong@chargefrog.com>
2020-11-18 10:42:11 +0800

    改变交互逻辑，变成折叠模式

--- pages/we_service/yuanshen_relic/role_setting.vue
+++ pages/we_service/yuanshen_relic/role_setting.vue
@@ -3,33 +3,39 @@
 		<block v-for="(item,index) in role" :key="index">
 			<u-gap height="48"></u-gap>
 				<view class="yuansu_box">
-					<view class="title" :style="{
+<!-- 					<view class="title" :style="{
 						'border-left':'6rpx solid '+item.color
 					}">
 						{{item.type_name}}
-					</view>
-					<view class="ren_box">
-						<view class="item" v-for="(x,y) in item.array" :key="y">
-							<chenyu-checkbox
-								@change="checkboxChange(index,y)"
-								:value="x.have"
-								:iconSize="14"
-								shape="fang"
-								primary-color="#18b566"/>
-							<view class="right">
-								<image @tap="checkboxChange(index,y)" style="width: 100rpx;height: 100rpx;" mode="widthFix" :src="'/pages/we_service/yuanshen_relic/static/'+x.icon+'.png'" />
-								<view class="text_name">
-									{{x.name}}
-								</view>
-								<u-rate v-if="x.have"
-									v-model="x.life"
-									active-color="#FA3534" 
-									inactive-color="#b2b2b2" 
-									:count="6"
-									gutter="0" />
+					</view> -->
+					<u-collapse>
+						<u-collapse-item :ref="'collapse_item'+index">
+							<view class="title" :style="{
+								'border-left':'6rpx solid '+item.color
+							}"
+							slot="title-all">
+								{{item.type_name}}
 							</view>
-						</view>
-					</view>
+							<view class="ren_box">
+								<view class="item" v-for="(x,y) in item.array" :key="y">
+									<view class="right">
+										<!-- <image @tap="checkboxChange(index,y)" style="width: 100rpx;height: 100rpx;" mode="widthFix" :src="'/pages/we_service/yuanshen_relic/static/'+x.icon+'.png'" /> -->
+										<image :class="x.have?'have_role':''" @tap="checkboxChange(index,y)" style="width: 100rpx;height: 100rpx;" mode="widthFix" src="/static/uni_logo.png" />
+										<view class="text_name">
+											{{x.name}}
+										</view>
+										<u-rate v-if="x.have"
+											v-model="x.life"
+											active-color="#FA3534" 
+											inactive-color="#b2b2b2" 
+											:count="6"
+											gutter="0" />
+									</view>
+								</view>
+							</view>
+						</u-collapse-item>
+					</u-collapse>
+					
 				</view>
 		</block>
 	</view>
@@ -56,7 +62,12 @@
 		methods:{
 			checkboxChange(index1,index2){
 				console.log(index1,index2)
+				
 				this.role[index1].array[index2].have = !this.role[index1].array[index2].have
+				this.$nextTick(()=>{
+					this.$refs['collapse_item'+index1][0].init()
+					console.log(this)
+				})
 			}
 		}
 	}
@@ -88,15 +99,17 @@
 				.item{
 					width: 33.3%;
 					margin-top: 20rpx;
-					padding-left: 20rpx;
 					display: flex;
 					align-items: center;
 					.right{
-						margin-left: 20rpx;
 						flex: 1;
 						display: flex;
 						flex-direction: column;
 						align-items: center;
+						.have_role{
+							border: 2rpx solid #FFD700;
+							box-shadow: 0rpx 0rpx 10rpx #FFD700;
+						}
 						.text_name{
 							font-size: 28rpx;
 							text-align: center;
